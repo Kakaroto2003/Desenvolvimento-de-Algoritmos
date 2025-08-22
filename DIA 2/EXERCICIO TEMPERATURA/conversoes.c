bcee21a2a83feaddbfc9b0446e4ffaa5162f903b
CaioAlexandre <caio.alexandre8000@gmail.com>
2025-08-21 21:35:50 -0300

    add

---
+++ DIA 2/EXERCICIO TEMPERATURA/conversoes.c
@@ -0,0 +1,2 @@
+//Desenvolva um sistema de conversões de unidades com menu interativo. Implemente conversões entre
+//Celsius/Fahrenheit, metros/quilômetros e segundos/minutos. Organize o código nos arquivos conversoes.h, conversoes.c e main.c.
---
+++ DIA 2/EXERCICIO TEMPERATURA/main.c
@@ -0,0 +1,4 @@
+//Desenvolva um sistema de conversões de unidades com menu interativo. Implemente conversões entre
+//Celsius/Fahrenheit, metros/quilômetros e segundos/minutos. Organize o código nos arquivos conversoes.h, conversoes.c e main.c.
+
+
