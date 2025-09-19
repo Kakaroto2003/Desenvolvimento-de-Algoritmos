9418bc4616f695dc78fe25071d2af44a2b9c961e
CaioAlexandre <caio.alexandre8000@gmail.com>
2025-09-18 21:30:36 -0300

    add

--- DIA 5/EXERCICIO_10.c
+++ DIA 5/EXERCICIO_10.c
@@ -3,3 +3,50 @@
 //▶ Leia duas matrizes 2x2
 //▶ Compare se são iguais (todos os elementos nas mesmas posições)
 //▶ Exiba “Matrizes iguais” ou “Matrizes diferentes”
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() {
+    int matriz1[2][2];
+    int matriz2[2][2];
+    int iguais = 1; // Assumimos que são iguais até que se prove o contrário
+
+    printf("Digite os elementos da primeira matriz 2x2:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("matriz1[%d][%d]: ", i + 1, j + 1);  // +1 para exibir a partir de 1
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    printf("\nDigite os elementos da segunda matriz 2x2:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("matriz2[%d][%d]: ", i + 1, j + 1);  // +1 para exibir a partir de 1
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    // Comparação das matrizes
+    for (int i = 0; i < 2 && iguais; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (matriz1[i][j] != matriz2[i][j]) {
+                iguais = 0; // São diferentes
+                break;
+            }
+        }
+    }
+
+    // Resultado
+    if (iguais) {
+        printf("\nMatrizes iguais\n");
+    } else {
+        printf("\nMatrizes diferentes\n");
+    }
+
+    system("pause");
+    return 0;
+}
+
+
--- DIA 5/EXERCICIO_9.c
+++ DIA 5/EXERCICIO_9.c
@@ -3,3 +3,29 @@
 //▶ Crie uma matriz 5x5
 //▶ Preencha com o padrão: matriz[i][j] = i + j
 //▶ Exiba a matriz resultante
+
+#include <stdio.h>
+
+int main() {
+    int matriz[5][5];
+
+    // Preenchendo a matriz com o padrão matriz[i][j] = i + j
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            matriz[i][j] = i + j;
+        }
+    }
+
+    // Exibindo a matriz
+    printf("Matriz 5x5 com padrão matriz[i][j] = i + j:\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%2d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    system("pause");
+    return 0;
+}
+
